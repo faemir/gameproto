b66739e005d9b108e66187949b595adedd077f34
mattblickem <explosivose@gmail.com>
2013-11-28 20:34:40 +0000

    initial commit Too much movement - need to restrict character movements Player control not yet added

---
+++ Assets/scripts/Dylan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dylan : MonoBehaviour 
+{
+
+	public JoustDirection direction = JoustDirection.LeftToRight;
+	public float endForce = 40f;
+
+	public enum JoustDirection { LeftToRight, RightToLeft };
+
+	private Rigidbody body;
+	private float force = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		body = transform.FindChild("Body").rigidbody;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 forceDirection = Vector3.zero;
+
+		if ( direction == JoustDirection.LeftToRight ) forceDirection = Vector3.forward;
+		else forceDirection = Vector3.back;
+
+		if ( force > endForce ) force = endForce;
+		body.AddForce(forceDirection * force);
+		force = force + Time.time;
+	}
+}
---
+++ Assets/scripts/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+	
+	public Transform target;
+	public Vector3 offset = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + offset;
+	}
+}
