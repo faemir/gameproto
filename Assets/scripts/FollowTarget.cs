b9b7e50805bf37421cd81a6a18c321081e8916ea
mattblickem <explosivose@gmail.com>
2013-11-28 23:29:35 +0000

    FollowTarget script now depends on target speed Makes a cool effect

--- Assets/scripts/FollowTarget.cs
+++ Assets/scripts/FollowTarget.cs
@@ -4,10 +4,24 @@ using System.Collections;
 public class FollowTarget : MonoBehaviour {
 	
 	public Transform target;
-	public Vector3 offset = Vector3.zero;
+	public float minHeight = 2f;
+	public float maxHeight = 10f;
+	public float maxSpeed = 20f;
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		Vector3 offset = Vector3.zero;
+		float speed = target.rigidbody.velocity.magnitude;
+		offset.y = FloatScale(0f, maxSpeed, minHeight, maxHeight, speed);
 		transform.position = target.position + offset;
+
+	}
+
+	float FloatScale(float oldMin, float oldMax, float newMin, float newMax, float value)
+	{
+		float oldRange = oldMax - oldMin;
+		float newRange = newMax - newMin;
+		return (((value - oldMin)*newRange)/oldRange)+newMin;
 	}
 }
