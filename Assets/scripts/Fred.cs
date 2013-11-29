2a6020b8d250ce25798619a26672f8fd9df9fb84
mattblickem <explosivose@gmail.com>
2013-11-29 00:14:26 +0000

    Added player control for jouster up/down will angle the lance

---
+++ Assets/scripts/Fred.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fred : MonoBehaviour 
+{
+
+	public float lanceTurnMultiplier = 0f;
+	private Rigidbody lance;
+	private float torque;
+	// Use this for initialization
+	void Start () 
+	{
+		lance = transform.FindChild("Lance").rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		torque = Input.GetAxis("Vertical") * lanceTurnMultiplier;
+		lance.AddTorque(0f, -torque, 0f, ForceMode.Acceleration);
+	}
+}
