d385022fbf7791a0de161fd0ca294e51e95706a9
Daniel Cohen <analoguecolour@gmail.com>
2013-12-17 00:41:49 +0000

    enemies partially done

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public Vector2 speed = new Vector2(20, 20);
+	public Vector2 direction = new Vector2(-10, -10);
+	public float moveForce = 10f;
+	public float maxSpeed = 0.5f;
+
+	public float alienSpeed = .02f;
+	public ScoreController score;
+	
+	private Vector2 movement;
+	private Transform groundDetector;
+	private bool grounded = false;
+
+	void start(){
+		score = GameObject.Find ("Score").GetComponent<ScoreController> ();
+	}
+
+	void awake(){
+		groundDetector = transform.Find("GroundDetectorEn");
+	}
+	void Update()
+	{
+		// The enemy is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		grounded = Physics2D.Linecast(transform.position, groundDetector.position, 1 << LayerMask.NameToLayer("Terrain"));
+
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+		
+		if (renderer.IsVisibleFrom(Camera.main) == false)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+	
+	void FixedUpdate()
+	{
+		if (grounded) {
+			rigidbody2D.velocity = movement;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the alien hits the bullet...
+		if (col.gameObject.tag == "Bullet") {
+			Debug.Log ("bullet");
+			score.score += 100;
+			
+		}
+	}
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -18,13 +18,17 @@ public class MovementController : MonoBehaviour
 	public Vector2 direction = new Vector2(-1, 0);
 	
 	private Vector2 movement;
+	private Transform groundDetector;
+	private bool grounded = false;
 
 	public float moveForce = 10f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 0.5f;				// The fastest the player can travel in the x axis.
-	
+
+	void awake(){
+		groundDetector = transform.Find("GroundDetectorEn");
+	}
 	void Update()
 	{
-
 		// 2 - Movement
 		movement = new Vector2(
 			speed.x * direction.x,
@@ -39,7 +43,9 @@ public class MovementController : MonoBehaviour
 	
 	void FixedUpdate()
 	{
-		// Apply movement to the rigidbody
-		rigidbody2D.velocity = movement;
+
+			// Apply movement to the rigidbody
+			rigidbody2D.velocity = movement;
+
 	}
 }
